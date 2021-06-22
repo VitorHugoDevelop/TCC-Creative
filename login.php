7f7e97b271e72fb2ada40305ac9f873e277a63f7
VitorHugoDevelop <vitorhugohrp@gmail.com>
2021-06-21 22:37:31 -0300

    Paginas de portugues Adicionadas

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login">
+        <form>
+            <label>Digite Seu Nome</label>
+            <input type="text" name="nome">
+            <label>Digite sua senha</label>
+            <input type="password" name="senha">
+            <input type="submit" Value="Entrar">
+        </form>
+    </div>
+    <div>
+        <p>É NOVO POR AQUI? CRIE UMA CONTA AGORA MESMO!</p>
+        <a href="#">Cadastrar</a>
+    </div>
+</body>
+</html>
