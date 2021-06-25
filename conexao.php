6330747b676f544d7f0912a8f8148e9e55b3455b
VitorHugoDevelop <vitorhugohrp@gmail.com>
2021-06-25 03:40:16 -0300

    adicionei as imagens

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+    $localhost = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "cadcriancas";
+
+    $conn = mysqli_connect($localhost, $user, $pass, $database) or die ("Não foi possivel conectar")
+
+
+?>
