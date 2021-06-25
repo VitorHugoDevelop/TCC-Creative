239a9f1f6112207aec5177f1918f51fda51bddf4
camelofon <vpersonoliveira@gmail.com>
2021-06-25 01:22:11 -0300

    Adicionado os logins
    finalmente subi isso

--- login.php
+++ login.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <div class="login">
-        <form>
-            <label>Digite Seu Nome</label>
-            <input type="text" name="nome">
-            <label>Digite sua senha</label>
-            <input type="password" name="senha">
-            <input type="submit" Value="Entrar">
-        </form>
-    </div>
-    <div>
-        <p>É NOVO POR AQUI? CRIE UMA CONTA AGORA MESMO!</p>
-        <a href="cadastrar.html">Cadastrar</a>
-    </div>
-</body>
-</html>
+<?php
+    include('conexao.php');
+
+    $usuario = $_post['usuario'];
+    $senha = $_post['senha'];
+
+
+?>
